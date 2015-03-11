ba46fe6eec82b35424688b795318920664e10dab
wlw <liweiwuhome@hotmail.com>
2015-03-11 22:44:51 +0800

    修改显示自己开放计划细节的ｂｕｇ

--- labmanageproject/my_decorator.py
+++ labmanageproject/my_decorator.py
@@ -14,7 +14,7 @@ def check_perm(func):
             t = t['url']
             pattern = re.compile(r'^' + t + '*')
             print "perm_t:%s" % t
-            print "request_path:%s" % request.path
+            # print "request_path:%s" % request.path
             if pattern.match(request.path):
                 return func(request, *args, **kwargs)
         else:
